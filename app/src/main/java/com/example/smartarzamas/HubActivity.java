e1401782965741d1fc14419e842929fde73ac445
IlyaKrn <ilyakornoukhov@gmail.com>
2022-01-31 23:59:26 +0300

    незначительные изменения

--- app/src/main/java/com/example/smartarzamas/HubActivity.java
+++ app/src/main/java/com/example/smartarzamas/HubActivity.java
@@ -75,25 +75,7 @@ public class HubActivity extends FirebaseActivity {
                 CallbackManager.callOnSearchStringChange(etSearch.getText().toString());
 
             }
-        });   /*.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-
-            }
-
-            @Override
-            public void afterTextChanged(Editable editable) {
-                CallbackManager.callOnSearchStringChange(etSearch.getText().toString());
-            }
         });
-
-        */
-
         btMenu.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
