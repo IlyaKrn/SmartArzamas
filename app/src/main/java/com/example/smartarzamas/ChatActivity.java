e13929f9f60af38a19dba2468706c6e83ed228d6
IlyaKrn <ilyakornoukhov@gmail.com>
2022-02-08 20:54:05 +0300

    пережод на активность с настройками чата

--- app/src/main/java/com/example/smartarzamas/ChatActivity.java
+++ app/src/main/java/com/example/smartarzamas/ChatActivity.java
@@ -1,18 +1,23 @@
 package com.example.smartarzamas;
 
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.widget.PopupMenu;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.smartarzamas.adapters.MessageListAdapter;
 import com.example.smartarzamas.firebaseobjects.Chat;
 import com.example.smartarzamas.firebaseobjects.Message;
+import com.example.smartarzamas.support.Category;
 import com.example.smartarzamas.support.Utils;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -32,6 +37,12 @@ public class ChatActivity extends FirebaseActivity {
     private ArrayList<Bitmap> imagesForSend  = new ArrayList<>();
     private Chat chat;
 
+    private ImageButton btSend;
+    private ImageButton btAddImage;
+    private ImageButton btChatSettings;
+    private ImageButton btClose;
+
+
     private ValueEventListener chatDataListener;
 
 
@@ -46,9 +57,13 @@ public class ChatActivity extends FirebaseActivity {
     // инициализация
     void init(){
         imagesForSend = new ArrayList<>();
-        tvName = (TextView)findViewById(R.id.tvName);
-        etSend = (EditText)findViewById(R.id.etSend);
+        tvName = (TextView)findViewById(R.id.tv_name);
+        etSend = (EditText)findViewById(R.id.et_send);
         rvMessages = findViewById(R.id.rv_messages);
+        btSend = findViewById(R.id.bt_send);
+        btAddImage = findViewById(R.id.bt_set_image);
+        btChatSettings = findViewById(R.id.bt_chat_menu);
+        btClose = findViewById(R.id.bt_close);
         rvMessages.setLayoutManager(new LinearLayoutManager(this));
         adapter = new MessageListAdapter(this, messageList,/* userList,new ArrayList<>(), */ user, new MessageListAdapter.OnStateClickListener() {
             @Override
@@ -58,6 +73,71 @@ public class ChatActivity extends FirebaseActivity {
         });
         rvMessages.setAdapter(adapter);
 
+        btSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (etSend.getText().toString().length() > 0) {
+                    String massege = etSend.getText().toString();
+                    if (messageList.get(0).equals(NULL_MESSAGE))
+                        messageList.remove(0);
+                    messageList.add(new Message(massege, user.id, dbChats.push().getKey(), imagesForSend));
+
+                    Utils.isConnected(getApplicationContext(), new Utils.Connection() {
+                        @Override
+                        public void isConnected() {
+                            dbChats.child(chat.id).addListenerForSingleValueEvent(new ValueEventListener() {
+                                @Override
+                                public void onDataChange(DataSnapshot dataSnapshot) {
+                                    dataSnapshot.child("messages").getRef().setValue(messageList);
+                                    scrollMessages();
+                                    etSend.setText("");
+                                }
+
+                                @Override
+                                public void onCancelled(DatabaseError databaseError) {
+                                }
+                            });
+                        }
+                    });
+
+                }
+            }
+        });
+        btAddImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        btChatSettings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                PopupMenu popup = new PopupMenu(ChatActivity.this, view);
+                popup.inflate(R.menu.popup_menu_chat);
+                popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem menuItem) {
+                        switch (menuItem.getItemId()) {
+                            // ямы на дорогах
+                            case R.id.chat_settings:
+                                Intent intent = new Intent(ChatActivity.this, ChatSettingsActivity.class);
+                                intent.putExtra(CHAT_ID, chat.id);
+                                intent.putExtra(USER_INTENT, user);
+                                startActivity(intent);
+                                break;
+                        }
+                        return false;
+                    }
+                });
+                popup.show();
+            }
+        });
+        btClose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
 
         chatDataListener = new ValueEventListener() {
             @Override
@@ -92,45 +172,8 @@ public class ChatActivity extends FirebaseActivity {
             }
         });
     }
-    // переход к списку чатов
-    public void onClose(View view) {
-        finish();
-    }
-    // отпрака сообщения
-    public void onSend(View view) {
-        if (etSend.getText().toString().length() > 0) {
-            String massege = etSend.getText().toString();
-            if (messageList.get(0).equals(NULL_MESSAGE))
-                messageList.remove(0);
-            messageList.add(new Message(massege, user.id, dbChats.push().getKey(), imagesForSend));
-
-            Utils.isConnected(getApplicationContext(), new Utils.Connection() {
-                @Override
-                public void isConnected() {
-                    dbChats.child(chat.id).addListenerForSingleValueEvent(new ValueEventListener() {
-                        @Override
-                        public void onDataChange(DataSnapshot dataSnapshot) {
-                            dataSnapshot.child("messages").getRef().setValue(messageList);
-                            scrollMessages();
-                            etSend.setText("");
-                        }
-
-                        @Override
-                        public void onCancelled(DatabaseError databaseError) {
-                        }
-                    });
-                }
-            });
-
-        }
-    }
     private void scrollMessages(){
         rvMessages.scrollToPosition(messageList.size()-1);
-    }
-    // открытие меню чата
-    public void onChatMenuOpen(View view) {
-    }
-    public void onAddImage(View view) {
     }
 
     @Override
