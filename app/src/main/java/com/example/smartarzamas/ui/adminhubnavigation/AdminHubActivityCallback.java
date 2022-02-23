bc7381904be1724a3f746756a4de273ebb228318
IlyaKrn <ilyakornoukhov@gmail.com>
2022-02-23 14:06:40 +0300

    ветка администратора с функционалом пользователя ОТКАТ

--- app/src/main/java/com/example/smartarzamas/ui/adminhubnavigation/AdminHubActivityCallback.java
+++
@@ -1,8 +0,0 @@
-package com.example.smartarzamas.ui.adminhubnavigation;
-
-import java.util.ArrayList;
-
-public interface AdminHubActivityCallback {
-    public void onCategoryChange(ArrayList<String> categories);
-    public void onSearchStringChange(String search);
-}
